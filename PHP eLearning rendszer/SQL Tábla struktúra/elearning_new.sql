9cfae7c6132ea3cbb7cc1352916ad3b42c27d74d
Máté András Miksa <58167256+mateandras0@users.noreply.github.com>
2023-05-07 10:06:42 +0200

    Update elearning_new.sql

--- PHP eLearning rendszer/SQL Tábla struktúra/elearning_new.sql
+++ PHP eLearning rendszer/SQL Tábla struktúra/elearning_new.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2.1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: May 06, 2023 at 09:08 PM
--- Server version: 5.7.33-0ubuntu0.16.04.1
--- PHP Version: 5.6.40-15+ubuntu16.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,7 +9,6 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `elearning_new`
 --
 
 -- --------------------------------------------------------
