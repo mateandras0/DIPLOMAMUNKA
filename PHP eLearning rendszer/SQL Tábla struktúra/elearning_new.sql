67589f83daa1fc99cbb2702f8a45ab199c41a40f
Máté András Miksa <58167256+mateandras0@users.noreply.github.com>
2023-05-07 10:05:20 +0200

    Add files via upload

---
+++ PHP eLearning rendszer/SQL Tábla struktúra/elearning_new.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 06, 2023 at 09:08 PM
+-- Server version: 5.7.33-0ubuntu0.16.04.1
+-- PHP Version: 5.6.40-15+ubuntu16.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `elearning_new`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `leader_lessons`
+--
+
+CREATE TABLE `leader_lessons` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `link` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lessons`
+--
+
+CREATE TABLE `lessons` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `group_name` varchar(100) NOT NULL,
+  `document` text NOT NULL,
+  `password` varchar(10) NOT NULL,
+  `towhom` varchar(100) NOT NULL,
+  `precondition` bigint(20) NOT NULL,
+  `min_points` tinyint(4) NOT NULL,
+  `question_number` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `other_lessons`
+--
+
+CREATE TABLE `other_lessons` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `link` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions`
+--
+
+CREATE TABLE `questions` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `lesson_id` bigint(20) NOT NULL,
+  `description` longtext NOT NULL,
+  `pic` text,
+  `type` varchar(20) NOT NULL,
+  `option1` text NOT NULL,
+  `option2` text NOT NULL,
+  `option3` text NOT NULL,
+  `option4` text NOT NULL,
+  `answer` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test_history`
+--
+
+CREATE TABLE `test_history` (
+  `id` bigint(20) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_tk8id` varchar(8) NOT NULL,
+  `lesson_id` bigint(20) NOT NULL,
+  `lesson_question_id` bigint(20) NOT NULL,
+  `lesson_question` text NOT NULL,
+  `correct_ans` varchar(10) NOT NULL,
+  `users_ans` varchar(10) NOT NULL,
+  `try` int(11) NOT NULL,
+  `success` tinyint(1) NOT NULL,
+  `up_date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `tk8id` varchar(8) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `lesson_id` bigint(20) NOT NULL,
+  `fill_date` date NOT NULL,
+  `result` varchar(10) NOT NULL,
+  `try_num` int(5) NOT NULL,
+  `finished` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `leader_lessons`
+--
+ALTER TABLE `leader_lessons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `lessons`
+--
+ALTER TABLE `lessons`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `other_lessons`
+--
+ALTER TABLE `other_lessons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
+
+--
+-- Indexes for table `test_history`
+--
+ALTER TABLE `test_history`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`tk8id`,`lesson_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `leader_lessons`
+--
+ALTER TABLE `leader_lessons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `lessons`
+--
+ALTER TABLE `lessons`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+--
+-- AUTO_INCREMENT for table `other_lessons`
+--
+ALTER TABLE `other_lessons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=687;
+--
+-- AUTO_INCREMENT for table `test_history`
+--
+ALTER TABLE `test_history`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12592;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
